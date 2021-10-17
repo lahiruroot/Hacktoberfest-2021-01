6fb35765909c1207bddb418602c439fa6208fae1
Pavithra De Silva <56332804+pndesilva@users.noreply.github.com>
2021-10-17 12:29:48 +0530

    Create management.js

---
+++ management.js
@@ -0,0 +1,22 @@
+<form name="selectForm">
+  <p>
+  </p>
+  <p><input id="btn" type="button" value="How many are selected?" /></p>
+</form>
+
+<script>
+function howMany(selectObject) {
+  let numberSelected = 0;
+  for (let i = 0; i < selectObject.options.length; i++) {
+    if (selectObject.options[i].selected) {
+      numberSelected++;
+    }
+  }
+  return numberSelected;
+}
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+  alert('Number of options selected: ' + howMany(document.selectForm.musicTypes));
+});
+</script>
